9096e47a472f172865d2d78bfe45f90bac3c8be4
Chetan H Lohar <chetan.lohar@softinfology.com>
2015-05-22 22:09:05 +0530

    commited

--- searchmytraining/src/main/java/com/searchmytraining/controller/LoginController.java
+++ searchmytraining/src/main/java/com/searchmytraining/controller/LoginController.java
@@ -8,34 +8,33 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-	 
-	@Controller
-	public class LoginController {
-		@Value("${name}")
-		String name;
-	 
-	@RequestMapping(value="/main", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model, Principal principal ) {
-	 
-	String name = principal.getName();
-	model.addAttribute("username", name);
-	return "main_page";
-	 
-	}
-	 
-	@RequestMapping(value="/login", method = RequestMethod.GET)
-	public String login(ModelMap model) {
-	 System.out.println(name);
-	return "login_page";
-	 
-	}
-	 
-	@RequestMapping(value="/loginError", method = RequestMethod.GET)
-	public String loginError(ModelMap model) {
-	model.addAttribute("error", "true");
-	return "login_page";
-	 
-	}
-	 
+@Controller
+public class LoginController {
+	@Value("${name}")
+	String name;
+
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model, Principal principal) {
+
+		String name = principal.getName();
+		model.addAttribute("username", name);
+		return "main_page";
+
 	}
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		System.out.println(name);
+		return "login_page";
+
+	}
+
+	@RequestMapping(value = "/loginError", method = RequestMethod.GET)
+	public String loginError(ModelMap model) {
+		System.out.println("in login error");
+		model.addAttribute("error", "true");
+		return "login_page";
+
+	}
+
+}
