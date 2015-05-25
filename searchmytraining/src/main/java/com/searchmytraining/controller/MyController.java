a15a4ad32bf5efa7222d62469053cef90a38ec6c
Prashant Nemade <prashant.nemade1@softinfology.com>
2015-05-25 11:16:20 +0530

    admin controller and MyController modified

--- searchmytraining/src/main/java/com/searchmytraining/controller/MyController.java
+++ searchmytraining/src/main/java/com/searchmytraining/controller/MyController.java
@@ -54,45 +54,10 @@ public class MyController {
 	{
 		return "index";
 	}
-	@RequestMapping("/AdminPages")
-	public String adminPage()
-	{		
-		return "pages/admin/AdminPages";		
-	}
-	@RequestMapping("/accountSetting")
-	public String acntSetting()
-	{		
-		return "pages/admin/AccountSetting";		
-	}
-	@RequestMapping("/manageCalender")
-	public String magCalender()
-	{		
-		return "pages/admin/ManageCalender";		
-	}
-	@RequestMapping("/manageTrainings")
-	public String manTraining()
-	{
-		return "pages/admin/ManageTrainings";
-	}
-	@RequestMapping("/manageServices")
-	public String manServices()
-	{
-		return "pages/admin/ManageServices";
-	}
-	@RequestMapping("/manageCareer")
-	public String manCareer()
-	{
-		return "pages/admin/ManageCareer";
-	}
 	@RequestMapping("/downlodCal")
 	public String GetCalenderDownload()
 	{
 		return "pages/downlodCalender";
 	}
 	
-	@RequestMapping("/admin")
-	public String getdminPage()
-	{
-		return "pages/AdminLogin";
-	}
 }
