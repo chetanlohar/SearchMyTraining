f9e81fbe70eb4c6d7263635220f379919a1b3c4f
Chetan H Lohar <chetan.lohar@softinfology.com>
2015-05-25 09:58:13 +0530

    fl

---
+++ searchmytraining/src/main/java/com/searchmytraining/dao/IFreeeLCertificationAwrdDAO.java
@@ -0,0 +1,7 @@
+package com.searchmytraining.dao;
+
+import com.searchmytraining.entity.CertificationAwardEntity;
+
+public interface IFreeeLCertificationAwrdDAO {
+	public void updateCertificationAwrdDet(CertificationAwardEntity certientity);
+}
---
+++ searchmytraining/src/main/java/com/searchmytraining/dto/CertificationAwardDTO.java
@@ -0,0 +1,19 @@
+package com.searchmytraining.dto;
+
+public class CertificationAwardDTO {
+	
+	private Integer userid;
+	private String awrdDetails;
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public String getAwrdDetails() {
+		return awrdDetails;
+	}
+	public void setAwrdDetails(String awrdDetails) {
+		this.awrdDetails = awrdDetails;
+	}
+}
