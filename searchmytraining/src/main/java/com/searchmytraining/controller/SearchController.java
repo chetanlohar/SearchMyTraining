ba2fa90ebb9fa9f0be79cd87bef8b7168a8031a1
Chetan H Lohar <chetan.lohar@softinfology.com>
2015-06-06 20:28:15 +0530

    added basic search dao for CalendarEntity

--- searchmytraining/src/main/java/com/searchmytraining/controller/SearchController.java
+++ searchmytraining/src/main/java/com/searchmytraining/controller/SearchController.java
@@ -1,6 +1,8 @@
 package com.searchmytraining.controller;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.searchmytraining.enquiry.training.SuggestTraining;
+import com.searchmytraining.entity.CalenderEntity;
+import com.searchmytraining.service.ICalenderService;
 
 @Controller
 public class SearchController {
+	
+	@Autowired
+	public ICalenderService calendarservice;
+	
+	
 	@RequestMapping(value="/dosearch",method = RequestMethod.GET)
 	@ResponseBody
 	public List<String> doSearch(@RequestParam("tagName") String tagName)
@@ -21,5 +30,14 @@ public class SearchController {
 		while(itr.hasNext())
 			System.out.println(itr.next());*/
 		return result;
+	}
+	
+	@RequestMapping(value = "/getCalendars", method = RequestMethod.POST)/*, produces = { "application/json" }, consumes = { "application/json" })*/
+	@ResponseBody
+	public List<CalenderEntity> getCalendars(@RequestParam("inputkeyword") String inputkeyword)
+	{
+		List<CalenderEntity> calendars =calendarservice.getCalendersByKeyword(inputkeyword); 
+		System.out.println(calendars.size());
+		return calendars;
 	}
 }
