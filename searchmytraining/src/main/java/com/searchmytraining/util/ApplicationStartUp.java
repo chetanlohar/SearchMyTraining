186c8169de64798508f86e30933e28208af22ded
Chetan H Lohar <chetan.lohar@softinfology.com>
2015-06-08 21:24:38 +0530

    calenderEntities retrived for search

--- searchmytraining/src/main/java/com/searchmytraining/util/ApplicationStartUp.java
+++ searchmytraining/src/main/java/com/searchmytraining/util/ApplicationStartUp.java
@@ -1,46 +1,44 @@
 package com.searchmytraining.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.List;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Index;
 import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.index.IndexableFieldType;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.BytesRef;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.searchmytraining.entity.CalenderEntity;
+import com.searchmytraining.service.ICalenderService;
 
-public class ApplicationStartUp implements ServletContextListener{
+@SuppressWarnings("deprecation")
+public class ApplicationStartUp implements ServletContextListener, HttpSessionAttributeListener, HttpSessionListener{
 
+	@Autowired
+	public ICalenderService calenderservice;
+	
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
+		System.out.println("From contextDestroyed() method");
 		
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent ctxEvent) {
-		System.out.println("Base Path is"+ctxEvent.getServletContext().getInitParameter("LuceneIndexFilePath"));
+		System.out.println("Base Path is: "+ctxEvent.getServletContext().getInitParameter("LuceneIndexFilePath"));
 		SearchUtil.basePath = ctxEvent.getServletContext().getInitParameter("LuceneIndexFilePath"); 
 		try{
-			indexTrainings();
+			indexTrainings(ctxEvent);
 		}catch(Exception ex){
 			System.out.println("Exception while indexing "+ex.getMessage());
 			ex.printStackTrace();
@@ -48,46 +46,101 @@ public class ApplicationStartUp implements ServletContextListener{
 				
 	}
 	
-	public void indexTrainings() throws Exception{
+	public void indexTrainings(ServletContextEvent ctxEvent) throws Exception{
 		SearchUtil util = new SearchUtil();
 		IndexWriter idx = util.getIndexforCalenderEntities();
+		WebApplicationContextUtils
+        .getRequiredWebApplicationContext(ctxEvent.getServletContext())
+        .getAutowireCapableBeanFactory()
+        .autowireBean(this);
 		
-		CalenderEntity cal1 = new CalenderEntity();
+		
+		List<CalenderEntity> calenders = calenderservice.getAllCalender();
+		
+		/*CalenderEntity cal1 = new CalenderEntity();
 		cal1.setDescription("Summer Java Training by NIIT");
 		cal1.setKeyword("JAVA, SQL, Hibernate, Multithreading");
 		cal1.setCode("Pune, Virar Mumbai, Delhi");
 		cal1.setContenttype("Classroom Training, Online Material");
-		
-		
 		CalenderEntity cal2 = new CalenderEntity();
 		cal2.setDescription("Personality Developement Training");
 		cal2.setKeyword("Personality Development, Soft Skills, Personality Improvement");
 		cal2.setCode("Hinjewadi, Pune, Ujjain");
-		cal2.setContenttype("Corporate Training");
+		cal2.setContenttype("Corporate Training");*/
 		
+		for(CalenderEntity calender: calenders)
+		{
+			Document calenderDoc = createDocument(calender);
+			idx.addDocument(calenderDoc);
+		}
 		
-		
-		Document d1 = createDocument(cal1);
+		/*Document d1 = createDocument(cal1);
 		Document d2 = createDocument(cal2);
-		
 		idx.addDocument(d1);
-		idx.addDocument(d2);
+		idx.addDocument(d2);*/
 		
 		idx.close();
 		
 	}
 
-	@SuppressWarnings("deprecation")
 	private Document createDocument(CalenderEntity cal){
 		Document doc = new Document();
-		IndexableField idxFl = new Field("KeyWord",cal.getKeyword(),Store.YES,Index.ANALYZED);
+		/*IndexableField idxFl = new Field("KeyWord",cal.getKeyword(),Store.YES,Index.ANALYZED);
 		doc.add(idxFl);
 		IndexableField idxF2 = new Field("Description",cal.getDescription(),Store.YES,Index.ANALYZED);
 		doc.add(idxF2);
 		IndexableField idxF3 = new Field("Code",cal.getCode(),Store.YES,Index.ANALYZED_NO_NORMS);
 		doc.add(idxF3);
 		IndexableField idxF4 = new Field("BasicSearchString",cal.getDescription() +" "+cal.getKeyword()+" "+cal.getCode(),Store.NO, Index.ANALYZED);
-		doc.add(idxF4);
+		doc.add(idxF4);*/
+		
+		IndexableField idxCalId = new Field("trngId",cal.getTrngId().toString(),Store.NO, Index.ANALYZED);
+		doc.add(idxCalId);
+		IndexableField idxTitle = new Field("title", cal.getTitle(), Store.YES, Index.ANALYZED);
+		doc.add(idxTitle);
+		IndexableField idxStartDate = new Field("start_date",cal.getStart_date(),Store.YES,Index.ANALYZED);
+		doc.add(idxStartDate);
+		IndexableField idxEndDate = new Field("end_date",cal.getEnd_date(),Store.YES,Index.ANALYZED);
+		doc.add(idxEndDate);
+		IndexableField idxPrice = new Field("price",cal.getPrice().toString(),Store.YES,Index.ANALYZED);
+		doc.add(idxPrice);
+		IndexableField idxPlace = new Field("place",cal.getPlace(),Store.YES,Index.ANALYZED);
+		doc.add(idxPlace);
+		IndexableField idxType = new Field("type",cal.getType(),Store.YES,Index.ANALYZED);
+		doc.add(idxType);
+		IndexableField idxDesc = new Field("description",cal.getDescription(),Store.NO,Index.ANALYZED);
+		doc.add(idxDesc);
+		
 		return doc;
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
